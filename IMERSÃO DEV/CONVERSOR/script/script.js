4624d2c5e957acfb434964f9b1c6d5c127ea785a
JeanDevJrAqui <jeangabrielpta2015@GMAIL.COM>
2025-03-31 20:35:14 -0300

    Adição do dolar e de formularios

--- IMERSÃO DEV/CONVERSOR/script/script.js
+++ IMERSÃO DEV/CONVERSOR/script/script.js
@@ -1,3 +1,25 @@
-won = 0.0040;
-valor = prompt("Digite o valor em wons: ");
-alert("o valor convertido em reais fica: " + valor * won);
+function converterWon() {
+    let won = 0.0040;
+    let valorWon = document.getElementById("valorWon").value;
+    let resultadoWonEl = document.getElementById("resultadoWon");
+
+    if (valorWon.trim() !== "" && !isNaN(valorWon)) {
+        let resultadoWon = parseFloat(valorWon) * won;
+        resultadoWonEl.value = `R$ ${resultadoWon.toFixed(2)}`;
+    } else {
+        resultadoWonEl.value = "Insira um valor válido";
+    }
+}
+
+function converterDolar() {
+    let dolar = 5.74;
+    let valorDolar = document.getElementById("valorDolar").value;
+    let resultadoDolarEl = document.getElementById("resultadoDolar");
+
+    if (valorDolar.trim() !== "" && !isNaN(valorDolar)) {
+        let resultadoDolar = parseFloat(valorDolar) * dolar;
+        resultadoDolarEl.value = `R$ ${resultadoDolar.toFixed(2)}`;
+    } else {
+        resultadoDolarEl.value = "Insira um valor válido";
+    }
+}
